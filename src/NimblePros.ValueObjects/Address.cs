7409f0225045c4ff560b3deef5e18042e4f28dbe
Steve Smith <steve@kentsmiths.com>
2022-01-23 21:43:02 -0500

    Add Address and simple test

---
+++ src/NimblePros.ValueObjects/Address.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+
+namespace NimblePros.ValueObjects;
+
+public class Address : ValueObject
+{
+  public String Street { get; private set; }
+  public String City { get; private set; }
+  public String State { get; private set; }
+  public String Country { get; private set; }
+  public String PostalCode { get; private set; }
+
+  public Address() { }
+
+  public Address(string street,
+    string city,
+    string state,
+    string country,
+    string postalCode)
+  {
+    Street = Guard.Against.NullOrEmpty(street, nameof(street));
+    City = Guard.Against.NullOrEmpty(city, nameof(city));
+    State = Guard.Against.NullOrEmpty(state, nameof(state));
+    Country = Guard.Against.NullOrEmpty(country, nameof(country));
+    PostalCode = Guard.Against.NullOrEmpty(postalCode, nameof(postalCode));
+  }
+
+  protected override IEnumerable<object> GetEqualityComponents()
+  {
+    // Using a yield return statement to return each element one at a time
+    yield return Street;
+    yield return City;
+    yield return State;
+    yield return Country;
+    yield return PostalCode;
+  }
+}
---
+++ tests/NimblePros.ValueObjects.UnitTests/AddressTests/AddressConstructor.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions.Execution;
+using Xunit;
+
+namespace NimblePros.ValueObjects.UnitTests.AddressTests;
+
+public class AddressConstructor
+{
+  private string _validCity = "city";
+  private string _validState = "state";
+  private string _validCountry = "country";
+  private string _validStreet = "street";
+  private string _validPostalCode = "postalCode";
+
+  [Fact]
+  public void SetsProperties()
+  {
+    var address = new Address(_validStreet, _validCity, _validState, _validCountry, _validPostalCode);
+
+    using(new AssertionScope())
+    {
+      Assert.Equal(_validStreet, address.Street);
+      Assert.Equal(_validCity, address.City);
+      Assert.Equal(_validState, address.State);
+      Assert.Equal(_validCountry, address.Country);
+      Assert.Equal(_validPostalCode, address.PostalCode);
+    }
+  }
+}
